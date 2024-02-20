943094ffdd9a9643768c55587d5da051b67435ab
Your Name <you@example.com>
2024-02-20 13:12:08 +0500

    second commit

--- app/ui/dashboard/pagination/pagination.tsx
+++ app/ui/dashboard/pagination/pagination.tsx
@@ -4,7 +4,7 @@ const Pagination = () => {
   return (
     <div className={styles.container}>
       <button className={styles.button} disabled>Previous</button>
-      <button className={styles.button} disabled>Next</button>
+      <button className={styles.button} >Next</button>
     </div>
   )
 }
