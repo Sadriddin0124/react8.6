e2c8ccc02d8debe76330bb603a7b1a77e8faa2b9
Your Name <you@example.com>
2024-02-22 13:49:18 +0500

    search added

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -4,52 +4,61 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Search from '@/app/ui/dashboard/search/search'
 import Pagination from '@/app/ui/dashboard/pagination/pagination'
-const Users = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.top}>
-        <Search placeholder="Search for a user..."/>
-        <Link href="/dashboard/users/add">
-          <button className={styles.addButton}>Add New</button>
-        </Link>
-      </div>
-      <table className={styles.table}>
-        <thead>
-          <tr>
-            <td>Name</td>
-            <td>Email</td>
-            <td>Created At</td>
-            <td>Role</td>
-            <td>Status</td>
-            <td>Actions</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <div className={styles.user}>
-              <Image src="/user.png" alt='user' width={50} height={50} className={styles.userImage}/>
-              John Doe
-              </div>
-              </td>
-            <td>john@gmail.com</td>
-            <td>20.02.2024</td>
-            <td>Admin</td>
-            <td>active</td>
-            <td>
-              <div className={styles.buttons}>
-                <Link href={`/dashboard/users/1`}>
-                  <button className={`${styles.button} ${styles.view}`}>View</button>
+import { fetchUsers } from '@/app/lib/data'
+
+const Users = async ({ searchParams }: { searchParams: { q: string } }) => {
+    const query = searchParams?.q || ""
+    const users = await fetchUsers(query)
+    console.log(users, "users");
+    return (
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <Search placeholder="Search for a user..." />
+                <Link href="/dashboard/users/add">
+                    <button className={styles.addButton}>Add New</button>
                 </Link>
-                  <button className={`${styles.button} ${styles.delete}`}>Delete</button>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <Pagination/>
-    </div>
-  )
+            </div>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>Created At</td>
+                        <td>Role</td>
+                        <td>Status</td>
+                        <td>Actions</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users?.map((item) => {
+                            return <tr key={item._id}>
+                                <td>
+                                    <div className={styles.user}>
+                                        <Image src={item?.img || "/user.png"} alt='user' width={50} height={50} className={styles.userImage} />
+                                        {item.username}
+                                    </div>
+                                </td>
+                                <td>{item?.email}</td>
+                                <td>{item?.createdAt?.toString()?.slice(4, 16)}</td>
+                                <td>{item?.isAdmin ? "Admin" : "Client"}</td>
+                                <td>{item?.isActive ? "Active" : "Passive"}</td>
+                                <td>
+                                    <div className={styles.buttons}>
+                                        <Link href={`/dashboard/users/1`}>
+                                            <button className={`${styles.button} ${styles.view}`}>View</button>
+                                        </Link>
+                                        <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                                    </div>
+                                </td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+            <Pagination />
+        </div>
+    )
 }
 
 export default Users
--- app/ui/dashboard/search/search.tsx
+++ app/ui/dashboard/search/search.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import styles from "./search.module.css"
-import { MdSearch } from 'react-icons/md'
-const Search = ({placeholder}: {placeholder: string}) => {
-  return (
-    <div className={styles.container}>
-      <MdSearch/>
-      <input type="text" placeholder={placeholder} className={styles.input}/>
-    </div>
-  )
-}
+"use client";
+import React, { ChangeEvent } from "react";
+import styles from "./search.module.css";
+import { MdSearch } from "react-icons/md";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-export default Search
+const Search = ({ placeholder }: { placeholder: string }) => {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const params = new URLSearchParams(searchParams);
+        params.set("q", e.target.value)
+        replace(`${pathname}?${params}`)
+    };
+    return (
+        <div className={styles.container}>
+            <MdSearch />
+            <input type="text" placeholder={placeholder} className={styles.input} onChange={handleChange} />
+        </div>
+    );
+};
+
+export default Search;
