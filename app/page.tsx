e8ebf641d0e6a9cde43133095139b943cb1f3282
Your Name <you@example.com>
2024-02-25 00:27:47 +0500

    dashboard link added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-[20px] p-24">
       <h1>Home</h1>
+      <Link href="/dashboard">Go to dashboard</Link>
     </main>
   );
 }
